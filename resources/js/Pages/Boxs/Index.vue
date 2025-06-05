505d9d7a547bdc86486489cb869cad1b0eb30ad8
KobalaGlou <louis.denis49@gmail.com>
2025-06-06 01:15:03 +0200

    fix test

--- resources/js/Pages/Boxs/Index.vue
+++ resources/js/Pages/Boxs/Index.vue
@@ -32,7 +32,7 @@ const hasBoxs = computed(() => props.boxs.length > 0)
           <div class="mb-6">
             <Link
               href="/boxs/create"
-              class="inline-block bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              class="inline-block bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
             >
               Ajouter une nouvelle box
             </Link>
